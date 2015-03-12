b38c70afaf4a3b1abd8f62a17b159c852e693806
Felipe Perez <felipe@chichasov.es>
2015-03-12 16:13:00 +0100

    Some debugging

--- main.py
+++ main.py
@@ -143,5 +143,6 @@ def arrestCriminal(captcha):
 
 if __name__ == '__main__':
     #app.debug = True
+    print 'Starting app . . .'
     serve(app, port=int(environ['PORT']))
     #app.run(port=8080, host='0.0.0.0', use_reloader=False) 
